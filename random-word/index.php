7fa57e15b43374aba1c1e374754dbfd6b245298a
Rick van der Wolk <rickvanderwolk@gmail.com>
2024-02-21 21:32:26 +0100

    Clean up

--- random-word/index.php
+++ random-word/index.php
@@ -5,7 +5,6 @@
     <title>Grrrandom word</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="one word, one request">
-    <meta name="author" content="Rick van der Wolk">
     <style>
         html,
         body {
--- uuid/index.php
+++ uuid/index.php
@@ -11,7 +11,6 @@ $uuid = Uuid::uuid4();
     <title>UUID</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="one UUID, one request">
-    <meta name="author" content="Rick van der Wolk">
     <style>
         html,
         body {
