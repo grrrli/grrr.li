667e6040e300a48ada8f1066ac7b8b2a1a582bc1
Rick van der Wolk <rickvanderwolk@gmail.com>
2024-02-24 23:06:09 +0100

    Set theme color (iOS status bar) color using prefers color scheme (light mode / dark mode)

--- helpers.php
+++ helpers.php
@@ -25,14 +25,16 @@ function renderHeadBase(
 ): void {
     ob_start();
     ?><meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#FFFFFF" media="(prefers-color-scheme: light)">
+    <meta name="theme-color" content="#000000" media="(prefers-color-scheme: dark)">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     <link rel="manifest" href="/site.webmanifest">
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="/assets/main.css"><?php
     $head = ob_get_clean();
