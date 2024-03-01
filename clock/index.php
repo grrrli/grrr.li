79f07384298419bb1a5752d4e904d92e8b617844
Rick van der Wolk <rickvanderwolk@gmail.com>
2024-03-01 18:50:45 +0100

    Add optional parameter format

--- clock/index.php
+++ clock/index.php
@@ -11,12 +11,32 @@
     <div id="clock-wrapper">
         <h1 id="clock"></h1>
     </div>
+
+    <?php
+    if (!empty($_GET['format'])) {
+        ?><script type="text/javascript" src="moment.js"></script><?php
+    }
+    ?>
+
     <script>
+        const queryParams = new URLSearchParams(window.location.search);
+        const format = queryParams.get('format') || null;
+
         const clockElement = document.getElementById('clock');
         let previousTime = null;
 
         updateClock();
         setInterval(updateClock, 100);
+
+        console.group('URL parameters');
+        console.group('optional');
+        console.group('format');
+        console.log('description: Moment.js format');
+        console.log('default: Date().toLocaleTimeString()');
+        console.log('example: ?format=YYYY-MM-DD');
+        console.groupEnd();
+        console.groupEnd();
+        console.groupEnd();
 
         function updateClock () {
             let time = getTime();
@@ -28,8 +48,12 @@
         }
 
         function getTime () {
-            const now = new Date();
-            return now.toLocaleTimeString();
+            if (format) {
+                return moment().format(format);
+            } else {
+                const now = new Date();
+                return now.toLocaleTimeString();
+            }
         }
     </script>
 </body>
