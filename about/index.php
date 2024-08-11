6a54e94bc48522e3ac236eb568891f5a584dae19
Rick van der Wolk <rickvanderwolk@gmail.com>
2024-08-11 11:32:17 +0200

    Update about page

--- about/index.php
+++ about/index.php
@@ -14,13 +14,13 @@
 >
 <h1>Grrr.li</h1>
 <h2>About</h2>
-<p>Born out of frustration, build with love.</p>
 <p>
-    My PC's fan shouldn't turn on when viewing a simple calendar, clock or random word website.
-    <br>
-    Such a page shouldn't need hundreds and hundreds of requests and multiple MB's of resources.
+    Simple things should be simple.
 </p>
-<p>Build for personal use but feel free to use it as well.</p>
+<p>
+    No cookies, no adds, not hundreds and hundreds of requests and multiple MBs of resources.
+</p>
+<p>Born out of frustration, build with love.</p>
 <br>
 <a
         href="/"
